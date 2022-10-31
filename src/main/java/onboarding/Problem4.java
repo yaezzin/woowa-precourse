79b79676b3d6c00fbe1f787f50ff51264b7403e0
yejin <yaezzin@gmail.com>
2022-10-31 18:28:33 +0900

    feat : 해당 문자가 대문자인지 판별하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    private static boolean isUpperAlphabet(int asciiCode) {
+        if (asciiCode >= 'A' && asciiCode <= 'Z') {
+            return true;
+        }
+        return false;
     }
 }

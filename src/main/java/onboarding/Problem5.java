a5e55c030c8d1a05072b9be99db00838c7ff79d2
yejin <yaezzin@gmail.com>
2022-10-31 21:22:52 +0900

    feat : 입력값을 검증하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,10 +9,18 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>();
         int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        if (!isValidNumber(money)) {
+            return Collections.emptyList();
+        }
+
         for (int i = 0; i < moneyList.length; i++) {
             answer.add(money / moneyList[i]);
             money %= moneyList[i];
         }
         return answer;
+    }
+
+    private static boolean isValidNumber(int money) {
+        return money >= 1 && money <= 1000000;
     }
 }

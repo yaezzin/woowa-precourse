b2fe028e8a645716fca4d8e2310f01d2e6f21059
yejin <yaezzin@gmail.com>
2022-10-31 21:02:13 +0900

    feat : problem3 숫자의 범위를 체크하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        if (!isNumberRange(number)) {
+            return -1;
+        }
         return answer;
+    }
+
+    private static boolean isNumberRange(int number) {
+        if (number < 1 || number > 10000) {
+            return false;
+        }
+        return true;
     }
 }

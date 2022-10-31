d467e68eb3cc3515e1a5f4ce63c8c0d29e25f35a
yejin <yaezzin@gmail.com>
2022-10-31 17:27:40 +0900

    feat : 리스트의 사이즈를 체크하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,19 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (pobi.size() != 2 || crong.size() != 2) {
+            return WrongListSizeException();
+        }
+
         return answer;
     }
+
+    private static Integer WrongListSizeException() {
+        return -1;
+    }
+
 }

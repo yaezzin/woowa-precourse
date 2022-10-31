3228589459ea95e323127cde71a429ae45f66ca9
yejin <yaezzin@gmail.com>
2022-10-31 18:31:45 +0900

    feat : 알파벳의 대소문자 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,10 +2,26 @@ package onboarding;
 
 public class Problem4 {
 
+    private static final int UPPER_ASCII_CODE = 65 + 90;
+    private static final int LOWER_ASCII_CODE_ = 97 + 122;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    private static char convertOppositeWord(char c) {
+        int currentChar = (int)c;
+
+        if (isUpperAlphabet(currentChar)) {
+            return (char)(UPPER_ASCII_CODE - currentChar);
+        }
+        if (isLowerAlphabet(currentChar)) {
+            return (char)(LOWER_ASCII_CODE_ - currentChar);
+        }
+        return c;
+    }
+
 
     private static boolean isUpperAlphabet(int asciiCode) {
         if (asciiCode >= 'A' && asciiCode <= 'Z') {

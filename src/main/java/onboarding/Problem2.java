61bddc37804d194acd4611685fe33f56e1afc618
yejin <yaezzin@gmail.com>
2022-11-01 01:11:16 +0900

    feat : 중복 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while (true) {
+            String decodeString = decodeString(cryptogram);
+            if (cryptogram.equals(decodeString)) {
+                   break;
+            }
+            cryptogram = decodeString;
+        }
+        return cryptogram;
     }
+
+    private static String decodeString(String cryptogram) {
+        for (int i = 0; i< cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                cryptogram = removeDuplicatedCharacter(cryptogram);
+            }
+        }
+        return cryptogram;
+    }
+
+    private static String removeDuplicatedCharacter(String cryptogram) {
+        return cryptogram.replaceAll("(([a-z])\\2{1,})", "");
+    }
+
 }

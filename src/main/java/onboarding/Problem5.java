bb926d51587d5b0710e18abb919749de1bdd8ef3
yejin <yaezzin@gmail.com>
2022-10-31 21:27:09 +0900

    refactor : solution 메서드에서 거스름돈 변환 메서드 추출

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,21 +6,25 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
-        int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-
         if (!isValidNumber(money)) {
             return Collections.emptyList();
         }
-
-        for (int i = 0; i < moneyList.length; i++) {
-            answer.add(money / moneyList[i]);
-            money %= moneyList[i];
-        }
-        return answer;
+        return changeMoney(money);
     }
 
     private static boolean isValidNumber(int money) {
         return money >= 1 && money <= 1000000;
     }
+
+    private static List<Integer> changeMoney(int money) {
+        List<Integer> answer = new ArrayList<>();
+        int[] moneyUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < moneyUnit.length; i++) {
+            answer.add(money / moneyUnit[i]);
+            money %= moneyUnit[i];
+        }
+        return answer;
+    }
+
 }

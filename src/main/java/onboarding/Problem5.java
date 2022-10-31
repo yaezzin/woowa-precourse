e77f437d092a08017b36df85fca19b0336fb24e3
yejin <yaezzin@gmail.com>
2022-10-31 21:20:22 +0900

    feat : 금액별 화폐 수를 저장하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < moneyList.length; i++) {
+            answer.add(money / moneyList[i]);
+            money %= moneyList[i];
+        }
         return answer;
     }
 }

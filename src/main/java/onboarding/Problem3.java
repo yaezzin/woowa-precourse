18c5c48634b45644df1bf4a1f0a8796f8cde308b
yejin <yaezzin@gmail.com>
2022-10-31 21:04:09 +0900

    feat : 3,6,9 개수를 세는 기능 구현 및 출력

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,7 +7,22 @@ public class Problem3 {
         if (!isNumberRange(number)) {
             return -1;
         }
+
+        for (int i = 1; i <= number; i++) {
+            answer += getCount(i);
+        }
         return answer;
+    }
+
+    private static int getCount(int number) {
+        int count = 0;
+        while (number != 0) {
+            if ((number % 10) == 3 || (number % 10) == 6 || (number % 10) == 9) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
     }
 
     private static boolean isNumberRange(int number) {

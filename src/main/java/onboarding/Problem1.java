e3245c92c891118e3a06c9e5a3789103cc2e22ec
yejin <yaezzin@gmail.com>
2022-10-31 17:29:50 +0900

    feat : 페이지 범위의 유효성을 체크하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 class Problem1 {
 
+    static final int MIN_PAGE = 1;
+    static final int MAX_PAGE = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
@@ -11,10 +14,30 @@ class Problem1 {
             return WrongListSizeException();
         }
 
+        Integer pobiLeftPage = pobi.get(0);
+        Integer pobiRightPage = pobi.get(1);
+        Integer crongLeftPage = crong.get(0);
+        Integer crongRightPage = crong.get(1);
+
+        if (!isPageRange(pobiLeftPage) || !isPageRange(pobiRightPage) || !isPageRange(crongLeftPage) || !isPageRange(crongRightPage)) {
+            return WrongPageRangeException();
+        }
+
         return answer;
     }
 
+    private static boolean isPageRange(Integer page) {
+        if (page > MAX_PAGE && page < MIN_PAGE) {
+            return false;
+        }
+        return true;
+    }
+
     private static Integer WrongListSizeException() {
+        return -1;
+    }
+
+    private static Integer WrongPageRangeException() {
         return -1;
     }
 

b39247608d602a9df7dee610dcb605ec4a8b33ff
yejin <yaezzin@gmail.com>
2022-11-01 18:07:00 +0900

    feat : 친구 관계 설정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,44 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    private static Map<String, List<String>> relations = new HashMap<>();
+    private static Map<String, Integer> score = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        getFriends(friends);
+        return null;
     }
+
+    private static void getFriends(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            String preFriend = friend.get(0);
+            String nextFriend = friend.get(1);
+
+            if (!relations.containsKey(preFriend)) {
+                setNewList(preFriend);
+            }
+            if (!relations.containsKey(nextFriend)) {
+                setNewList(nextFriend);
+            }
+            setRelations(preFriend, nextFriend);
+        }
+    }
+
+    private static void setNewList(String friend) {
+        List<String> friendList = new ArrayList<>();
+        score.put(friend, 0);
+        relations.put(friend, friendList);
+    }
+
+    private static void setRelations(String preFriend, String nextFriend) {
+        List<String> preFriendList = relations.get(preFriend);
+        preFriendList.add(nextFriend);
+
+        List<String> nextFriendList = relations.get(nextFriend);
+        nextFriendList.add(preFriend);
+    };
+
 }

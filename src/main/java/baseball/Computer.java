1e98e43249c534d0b505652aa501436a0fa38d4c
yejin <yaezzin@gmail.com>
2022-11-08 16:17:38 +0900

    feat : add a method to print result

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -46,7 +46,20 @@ public class Computer {
                 ball++;
             }
         }
+        getResult();
+
         return strike;
     }
 
+    public static void getResult() {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+    }
 }

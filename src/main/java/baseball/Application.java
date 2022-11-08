848b190eeb4ee52c8fab7a86bbfc5f45b2559a06
yejin <yaezzin@gmail.com>
2022-11-08 12:24:13 +0900

    feat : add a method to generate a random three digit number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static List<Integer> computer = new ArrayList<>();
+
     private static void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        generateRandomNumber();
+    }
+
+    private static void generateRandomNumber() {
+        computer.clear();
+        while(computer.size() < 3) {
+            int randomNUmber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNUmber)) {
+                computer.add(randomNUmber);
+            }
+        }
     }
 
     public static void main(String[] args) {

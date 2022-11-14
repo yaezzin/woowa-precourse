2eec0c6fa4f6e053f7085c2afae246499f9650ac
yejin <yaezzin@gmail.com>
2022-11-14 23:43:31 +0900

    feat : 사용자의 구매금액을 입력 받는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static int inputUserPay() {
+        System.out.println("구입금액을 입력해 주세요");
+        String money = Console.readLine().trim();
+        return Integer.parseInt(money);
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
 }

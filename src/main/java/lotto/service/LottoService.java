3eb5b0cd5f3254fb5df6576635fe44353bfde03a
yejin <yaezzin@gmail.com>
2022-11-15 17:05:06 +0900

    feat : 당첨 기준에 맞게 당첨 내역을 정리하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,23 +2,37 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.LottoWinType;
 import lotto.utils.ValidateUtils;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import static lotto.domain.LottoWinType.matchGameScoreAndLottoWinType;
+
 public class LottoService {
 
     public void startLottoGame() {
         // 1. 사용자의 구매금액을 입력 받음
-        inputUserPay();
+        int userPay = inputUserPay();
 
-        // 2. 사용자의 숫자를 입력받음
+        // 2. 로또 개수를 출력
+        int lottoCount = getLottoCount(userPay);
+
+        // 3. 랜덤한 숫자를 뽑음
+        makeRandomLottoNumbers();
+
+        // 4. 로또를 구매함
+        buyLotto(lottoCount);
+
+        // 5. 사용자의 숫자를 입력받음
         inputLottoNumbers();
 
-        // 3. 보너스 번호를 입력받음
+        // 6. 보너스 번호를 입력받음
         inputBonusLottoNumber();
+
+
     }
 
     private static int inputUserPay() {
@@ -31,18 +45,13 @@ public class LottoService {
         return Integer.parseInt(money);
     }
 
-    public static int getLottoCount(int money) {
+    private static int getLottoCount(int money) {
         int change = 0;
         change = money / 1000;
         System.out.println(change + "개 를 구매했습니다.");
         return change;
     }
 
-    public static List<Integer> makeRandomLottoNumbers() {
-        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        numbers.sort(Comparator.naturalOrder());
-        return numbers;
-    }
 
     private static List<List<Integer>> buyLotto(int change) {
         List<List<Integer>> lottoNumberList= new ArrayList<>();
@@ -54,6 +63,12 @@ public class LottoService {
             System.out.println(lotto);
         }
         return lottoNumberList;
+    }
+
+    private static List<Integer> makeRandomLottoNumbers() {
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        numbers.sort(Comparator.naturalOrder());
+        return numbers;
     }
 
     private static List<Integer> inputLottoNumbers() {
@@ -72,11 +87,27 @@ public class LottoService {
         return answer;
     }
 
-    public static int inputBonusLottoNumber() {
+    private static int inputBonusLottoNumber() {
         String bonus = Console.readLine().trim();
         ValidateUtils.checkNumberType(bonus);
         ValidateUtils.checkInputSize(bonus.length(), 1);
         ValidateUtils.checkNumberRange(Integer.parseInt(bonus));
         return Integer.parseInt(bonus);
     }
+
+    private static LottoWinType checkSameNumber(List<Integer> lotto, List<Integer> winLotto, int bonusLotto) {
+        int answerCount = 0;
+        int bonusCount = 0;
+
+        for (Integer number : lotto) {
+            if (winLotto.contains(number)) {
+                answerCount++;
+            }
+            if (number == bonusLotto) {
+                bonusCount++;
+            }
+        }
+        return matchGameScoreAndLottoWinType(answerCount, bonusCount);
+    }
+
 }

fad433720886631ecabcb43bc8a6c7ddac74b507
yejin <yaezzin@gmail.com>
2022-11-15 22:37:54 +0900

    test : 맞은 개수와 보너스 정답 유무를 확인하는 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoWinType;
+import lotto.service.LottoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -19,10 +22,18 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("맞은 개수와 보너스 정답 유무를 확인한다.")
+    @Test
+    void checkHitCountAndBonusHitCount2() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = List.of(1,2,5,7,9,10);
+        int bonus = 6;
+        LottoWinType lottoWinType = lotto.checkMatchedLotto(winningNumbers, bonus);
+        assertThat(lottoWinType).isEqualTo(LottoWinType.FIFTH);
+    }
+
 }

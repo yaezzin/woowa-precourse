b996aaa13c174378c3a0c4317a512217a5e8edb4
yejin <yaezzin@gmail.com>
2022-11-22 14:23:44 +0900

    feat : 입력받은 길이에 해당하는 모양을 생성하는 기능 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,21 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int randomNumber = bridgeNumberGenerator.generate();
+            String board = convertRandomNumberToBoard(randomNumber);
+        }
+        return bridge;
+    }
+
+    private String convertRandomNumberToBoard(int randomNumber) {
+        if (randomNumber == 0) {
+            return "D";
+        }
+        if (randomNumber == 1) {
+            return "U";
+        }
+        throw new IllegalArgumentException();
     }
 }

38fc686102ecf288bbaae09e68f5f8acabc0b4c0
yejin <yaezzin@gmail.com>
2022-11-22 17:55:34 +0900

    test : 다리 이동 문자열 입력 테스트

--- src/test/java/bridge/ValidateTest.java
+++ src/test/java/bridge/ValidateTest.java
@@ -13,6 +13,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ValidateTest extends NsTest {
 
+    @DisplayName("올바르지 않은 다리 이동 문자열인 경우 예외를 출력한다.")
+    void 다리_이동_문자열_입력_예외_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "Q");
+            assertThat(output()).contains(ErrorMessage.INPUT_MOVE_TYPE_ERROR);
+        }, 1, 0, 0);
+
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "C");
+            assertThat(output()).contains(ErrorMessage.INPUT_MOVE_TYPE_ERROR);
+        }, 1, 0, 0);
+    }
+
     @DisplayName("다리의 길이가 정수가 아닌 값일 경우 예외를 출력한다.")
     @Test
     void 다리의_길이_입력_테스트() {
@@ -26,6 +39,21 @@ public class ValidateTest extends NsTest {
             assertThat(output()).contains(ErrorMessage.VALIDATE_INTEGER_TYPE_ERROR);
         }, 1);
     }
+
+    @DisplayName("다리의 길이가 3 이상 20 이하의 값이 아닌 경우 예외를 출력한다.")
+    @Test
+    void 다리의_길이_범위_입력_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("0");
+            assertThat(output()).contains(ErrorMessage.INPUT_NUMBER_RANGE_ERROR);
+        }, 1);
+
+        assertRandomNumberInRangeTest(() -> {
+            run("1000");
+            assertThat(output()).contains(ErrorMessage.INPUT_NUMBER_RANGE_ERROR);
+        }, 1);
+    }
+
 
     @Override
     protected void runMain() {

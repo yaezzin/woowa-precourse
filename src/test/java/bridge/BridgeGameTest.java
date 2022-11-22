0cebea7343b76c7b52ef31c76d34a5d36b9f42b3
yejin <yaezzin@gmail.com>
2022-11-22 18:00:16 +0900

    test : 잘못된 칸을 이동했는지 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,4 +1,21 @@
 package bridge;
 
+import bridge.service.BridgeGame;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BridgeGameTest {
+
+    @Test
+    void 잘못된_칸_이동_테스트() {
+        List<String> bridge = new ArrayList<>(List.of("U","D","D"));
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        Assertions.assertThat(bridgeGame.move("D")).isEqualTo(false);
+    }
+
+
+
 }

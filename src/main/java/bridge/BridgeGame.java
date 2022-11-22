c6f6d14b9c20933b54e64947bf2d5a79e43a3829
yejin <yaezzin@gmail.com>
2022-11-22 14:49:46 +0900

    feat : 게임 재시도를 위한 retry() 메서드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -15,6 +15,7 @@ public class BridgeGame {
     private final List<String> downBoardList = new ArrayList<>();
     private int count = 0;
     private boolean success = false;
+    private int tryCount = 1;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -73,5 +74,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        this.boardList.clear();
+        this.upBoardList.clear();
+        this.downBoardList.clear();
+        this.count = 0;
+        this.tryCount += 1;
+        this.success = true;
     }
 }

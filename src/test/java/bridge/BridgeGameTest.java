421a8896bfa43918b8666046fd078c384453f4eb
yejin <yaezzin@gmail.com>
2022-11-22 18:07:38 +0900

    test : 날못된 칸 이동 후 실패했는지를 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -11,7 +11,7 @@ public class BridgeGameTest {
 
     @Test
     void 잘못된_칸_이동_테스트() {
-        List<String> bridge = new ArrayList<>(List.of("U","D","D"));
+        List<String> bridge = new ArrayList<>(List.of("U","D","Ugit "));
         BridgeGame bridgeGame = new BridgeGame(bridge);
         Assertions.assertThat(bridgeGame.move("D")).isEqualTo(false);
     }
@@ -24,7 +24,7 @@ public class BridgeGameTest {
     }
 
     @Test
-    void 올바른_칸_이동_후_성공() {
+    void 올바른_칸_이동_후_성공_테스트() {
         List<String> bridge = new ArrayList<>(List.of("U","U","D"));
         BridgeGame bridgeGame = new BridgeGame(bridge);
         bridgeGame.move("U");
@@ -33,6 +33,13 @@ public class BridgeGameTest {
         Assertions.assertThat(bridgeGame.getSuccess()).isEqualTo(true);
     }
 
-
-
+    @Test
+    void 잘못된_칸_이동_후_실패_테스트() {
+        List<String> bridge = new ArrayList<>(List.of("U","D","D"));
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        bridgeGame.move("U");
+        bridgeGame.move("D");
+        bridgeGame.move("U");
+        Assertions.assertThat(bridgeGame.getSuccess()).isEqualTo(false);
+    }
 }
